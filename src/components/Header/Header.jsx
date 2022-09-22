a37cad723327a501c99b964a0539c68d1db3887b
Ana Santos <anabia200113@gmail.com>
2022-09-22 14:14:28 -0300

    deu certo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,10 +5,9 @@ function Header(){
         <header>
             <div className={styles.header}>
                 <ul className={styles.list}>
-                    <li><a href='#home'>Home</a></li>
-                    <li><a href='#about'>About</a></li>
-                    <li><a href='#comments'>Comments</a></li>
-                    <li><a href='#contact'>Contact</a></li>
+                    <li><a href="/">About</a></li>
+                    <li><a href="/comments">Comments</a></li>
+                    <li><a href="/contact">Contact</a></li>
                 </ul>
             </div>
             <hr></hr>
