43c6f05309ee9cfc5d412c29a3e3936ab9a34fda
Ana Santos <anabia200113@gmail.com>
2022-09-25 17:17:36 -0300

    quase lá

--- src/pages/Comments/Comments.jsx
+++ src/pages/Comments/Comments.jsx
@@ -1,20 +1,66 @@
 import styles from './comments.module.css'
-import { FiPlus, FiTrash } from 'react-icons/fi'
+import { FiPlus, FiTrash2 } from 'react-icons/fi'
 import { nanoid } from 'nanoid'
+import { useState, useEffect } from 'react'
 
-const itemsList = [
-    {
-        id: nanoid(),
-        title: "Seu comentário aparecerá desta forma."
-    },
-    {
-        id: nanoid(),
-        title: "Deixe o seu comentário."
+
+// como se lê: react vai no localStorage e verifica se há uma list, caso sim traga p/ mim.
+const Comments = () => {
+    const getLocalList = () => {
+    let items = localStorage.getItem('list') // essa list é string do localStorage, n confundir com a outra
+                // são do JS 
+
+    // se esses itens já existirem, me retorne no formato JSON 
+    if (items) {         // memória do MEU computador
+        return JSON.parse(localStorage.getItem('list'))
+    } else {
+        return [] // se n tiver me retorne um array vazio
+      }
     }
-]
 
+    const [list, setList] = useState(getLocalList)
+    const [newTask, setNewTask] = useState('')  // começará como uma lista vazia
 
-function Comments (){
+    // function handleInput(event) {
+    //     setTask(event.target.value)
+    // }
+
+    function handleCreateNewTask () {
+        const task = {
+            id: nanoid(), // criação de forma dinâmica pegando o que vai ser preenchido pelo usuário
+            title: newTask, // estado de task (linha 23)
+            isComplete: false
+        }
+
+        // n deixar criar se estiver vazio
+        if(task.title === '') {
+            return
+        }
+
+//como se lê: "minha lista vai receber tudo que já EXISTIA [...list] dentro da list + a nova tarefa. Isso me permite criar um novo item sem sobreescrevê-lo (como se fosse a iteração do for)."
+// nome técnico das reticências: spread operator   -- JS PURO
+        setList([...list, task])
+        setNewTask('') // deixar a task vazia dps que for criada
+        }
+
+        
+        // remova uma task da listagem pelo id
+        function handleRemoveTask(id) {
+
+            // faça um filtro e me traga o que for diferente daquele item que cliquei e reconheceu o id
+            const tasksFiltered = list.filter(task => task.id !== id)
+                                  // olhe p/ o id da lista de um em um e retorne o que for diferente
+            setList(tasksFiltered) // a lista filtrada
+        }
+        
+        // LOCALSTORAGE SÓ ENTENDE STRING
+        // função p/ salvar na local storage.
+        useEffect(() => {
+            // cria um localStorage p/ mim colocando os itens
+            localStorage.setItem('list', JSON.stringify(list)) // fazendo transformação ao contrário.
+        }, [list])
+        
+
     return (
         <>
             <div className={styles.header}>
@@ -26,23 +72,27 @@ function Comments (){
                     className={styles.input}
                     type="text"
                     placeholder="Digite sua mensagem aqui"
+                    onChange={(e) => setNewTask(e.target.value)}
+                    value={newTask}
                 />
 
                 <div className={styles.addContainer}>
-                    <button className={styles.addTask}>
-                        <FiPlus size={30} />
+                    <button className={styles.addTask} type="submit" onClick={handleCreateNewTask}>
+                        <FiPlus size={30} color="#fff" />
                     </button>
                 </div>
             </div>
 
             <main>
                 <ul className={styles.itemsList}>
-                    {itemsList.map(item => {
+                    {list.map(task => {
                         return (
-                            <li>
-                                    <p>{item.title}</p>
-                                    <button className={styles.removeTask}>
-                                        <FiTrash size={20} />
+                            <li key={task.id}>
+                                <div>
+                                <p>{task.title}</p>
+                                </div>                                                                            
+                                    <button className={styles.removeTask} type="button" data-testid="remove-task" onClick={() => handleRemoveTask(task.id)}> 
+                                        <FiTrash2 size={20} />
                                     </button>
                             </li>
                         )
@@ -50,7 +100,11 @@ function Comments (){
                 </ul>
             </main>
         </>
+
     )
 }
 
 export default Comments
+
+// explicação da linha 89:  sempre qnd está passando uma função, no caso handleRemoveTask,(mas poderia ser qqr evento) que tem PARÂMETRO ela será o retorno de uma arrow function, caso contrário quebra.
+// se n tiver parâmetro é só passar o nome direto, como onClick={handleCreateNewTask}
--- src/pages/Jornada/Jornada.jsx
+++ src/pages/Jornada/Jornada.jsx
@@ -3,11 +3,55 @@ import styles from './jornada.module.css'
 function Jornada(){
     return(
         <>
-            <h2>Sobre mim</h2>
-            <p>quem sou eu</p>
-            <h2>Jornada em tech</h2>
-            <p>caminho das comunidades digitais</p>
+            <div className={styles.sobre}>
+                <h2>Sobre mim</h2>
+            </div>
+            <div className={styles.text}>
+                <p>Meu nome é Ana Beatriz, mas prefiro Ana porque sinto que estão brigando comigo quando me chamam pelo nome composto. Sou fascinada pela perspectiva <span>interdisciplinar</span>, isto é, acredito na interconexão dos conhecimentos, e estou no processo de continuidade de carreira, ouvi isso de uma mentora maravilhosa e resolvi acatar, pois achei linda a forma como o repertório é valorizado como parte da pessoa desenvolvedora. </p>
+            </div>
+
+            <div className={styles.sobre}>
+                <h2>Jornada em tech</h2>
+            </div>
+
+            <div className={styles.text}>
+                <p>Estar no processo de continuidade de carreira para a área da tecnologia é algo que verdadeiramente mexe com as estruturas da pessoa que decide trilhar esse caminho permeado por constantes desafios. Contudo, os frutos que são colhidos durante o percurso é o que traz significado à trajetória. </p>
+                <p>Dado pressuposto, vou procurar sintetizar em poucas palavras esse percurso que se iniciou no mês de abril, no evento <span>Programação para não programadores</span> promovido pelo Movimento Black Money e ministrado pela desenvolvedora Simara Conceição.</p>
             
+            <br></br>
+            <p>Vieses algoritmicos - envolvimento dentro da universidade com uma prof que mudou a minha vida (Marina)</p>
+            <br></br>
+            <p>inquietude de conseguir visualizar como as humanidades se relacionam intimamente com a tecnologia e continuarem separando em áreas isoladas como se fossem conhecimento dissonantes.</p>
+
+            </div>
+
+            <div className={styles.sobre}>
+                <h2>Vínculo com as comunidades digitais</h2>
+            </div>
+
+            <div className={styles.text}>
+                <p>Certamente as comunidades digitais tiveram um papel crucial para que o impulso fosse tomado e acredito que palavras não seriam suficientes para expressar tamanha gratidão, em especial às comunidades compostas por pessoas negras. </p>
+            </div>
+
+            <div className={styles.sobre}>
+                <h2><i>Redescobrir o gosto e o sabor da festa</i></h2>
+                <h5>Verso da música <span>Redescobrir</span> (amo a versão na voz da Maria Rita, filha da Elis Regina).</h5>
+            </div>
+
+            <div className={styles.text}>
+                <p>Trilhar esse percurso de aprendizagem em uma área completamente nova é desafiador, pois tive de mudar a minha mentalidade e ser muito persistente, orientando a minha perspectiva para o futuro sem deixar de colher os frutos do presente. Se fosse para me resumir em uma frase certamente seria: “a Ana vê potencialidade na diversidade e na curiosidade tb (cá pra nós)”.  </p>
+            </div>
+
+            <div className={styles.video}>
+                <h2>Além disso, não poderia deixar de lado a música <span>Quase lá</span>, que alimenta continuamente a minha serelepe criança interior a acreditar no processo. </h2>
+                <iframe width="880" height="500" src="https://www.youtube.com/embed/43kEjzHpgTM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+
+            {/* <div className={styles.video}>
+                <h2>Certamente os versos da música <span>Redescobrir</span> delinearam cada pedacinho dessa jornada, que tal dar uma rememorada nesta melodia contagiante?</h2>
+                <iframe width="880" height="500" src="https://www.youtube.com/embed/P39f0_aYv-Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>  */}
+
         </>
     )
 }
