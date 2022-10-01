7f21d60b491c263b300c426ae402006a6ec1a9f5
Ana Santos <anabia200113@gmail.com>
2022-10-01 15:47:07 -0300

    text

--- src/pages/Jornada/Jornada.jsx
+++ src/pages/Jornada/Jornada.jsx
@@ -11,8 +11,8 @@ function Jornada(){
                 <h2>Sobre mim</h2>
             </div>
             <div className={styles.texto}>
-                <p>Meu nome é Ana Beatriz, mas prefiro Ana porque sinto que estão brigando comigo quando me chamam pelo nome composto (paranoia dos que tem nome composto). Sou fascinada pela perspectiva <span>interdisciplinar</span>, isto é, acredito na interconexão dos conhecimentos, e estou no processo de continuidade de carreira, ouvi isso de uma mentora maravilhosa e resolvi acatar, pois achei linda a forma como o repertório é valorizado como parte da pessoa desenvolvedora. </p>
-                <p>Trago como repertório minha formação não concluída na área das humanidades na Universidade Federal de São Paulo (UNIFESP), jornada esta não encerrada porque me fascinei pela tecnologia e decidi redirecionar minha vida para o que faz meus olhos brilharem com perspectiva. Adoraria falar que sempre tive aptidão para a área tecnológica e que desmontei um computador com meu pai aos 5 anos de idade, mas estaria falseando a realidade. Na verdade, apenas agora em 2022 aos 21 anos de idade (favor respeitar a geração 2001) consigo visualizar <span>futuros possíveis</span> para o meu corpo em determinados espaços. </p>
+                <p>Meu nome é Ana Beatriz, sou fascinada pela perspectiva <span>interdisciplinar</span>, isto é, acredito na interconexão dos conhecimentos, e estou no processo de continuidade de carreira, ouvi isso de uma mentora maravilhosa e resolvi acatar, pois achei linda a forma como o repertório é valorizado como parte da pessoa desenvolvedora. </p>
+                <p>Trago como repertório minha graduação não concluída em humanidades na Universidade Federal de São Paulo (UNIFESP), jornada esta não selada porque me fascinei pela área tecnológica nos últimos meses e decidi redirecionar minha vida para o que faz meus olhos brilharem com perspectiva. Adoraria falar que sempre tive aptidão para a área e que desmontei um computador com meu pai aos 5 anos de idade, mas estaria falseando a realidade. Na verdade, apenas agora em 2022, aos 21 anos de idade (favor respeitar a geração 2001), consigo visualizar <span>futuros possíveis</span> para o meu corpo em determinados espaços. </p>
 
             </div>
 
@@ -21,11 +21,13 @@ function Jornada(){
             </div>
 
             <div className={styles.texto}>
-                <p>Estar no processo de continuidade de carreira para a área da tecnologia é algo que verdadeiramente mexe com as estruturas da pessoa que decide trilhar esse caminho permeado por constantes desafios. Contudo, os frutos que são colhidos durante o percurso é o que traz significado à trajetória. Dado pressuposto, vou procurar sintetizar em poucas palavras esse percurso que se iniciou no mês de abril, no evento <span>Programação para não programadores</span> promovido pelo Movimento Black Money e ministrado pela desenvolvedora Simara Conceição.</p>
-                <p>Após ter pesquisado muito sobre o tema e revisitado diversos pontos de vista, indo de tecnologistas à cientistas sociais, encorpei minha questão formulada inicialmente, todavia, ainda não tenho as respostas para as minhas incógnitas.  Contudo, a possibilidade de beber da fonte de outras áreas para a analisar um fenômeno verdadeiramente me instigou. 
-                    <span> Pense em um mosaico</span>. É exatamente dessa forma que eu enxergo a construção interdisciplinar, pois apesar de cada elemento possuir suas características próprias é na harmonia (às vezes nem tão harmoniosa) que algo muito mais robusto é gerado. É na conexão que pode-se alcançar a <span>inovação</span> por meio de <span>valores ressignificados</span>, e é exatamente isso que faz os meus olhos brilharem na área tecnológica. </p>
+                <p>Estar no processo de continuidade de carreira para a área da tecnologia é algo que verdadeiramente mexe com as estruturas da pessoa que decide trilhar esse caminho permeado por constantes desafios. Contudo, os frutos que são colhidos durante o percurso é o que traz significado à trajetória. Dado pressuposto, vou procurar sintetizar em poucas palavras esse percurso que se iniciou no mês de abril, no evento <span>Programação para não programadores</span>, promovido pelo Movimento Black Money e ministrado pela desenvolvedora Simara Conceição.</p>
 
-                <p>Em suma, posso considerar que no meu tempo livre gosto de estudar sobre algoritmos e inteligencia artificial, com ênfase nos vieses de reconhecimento facial. Contudo, o que realmente me cativou foi a área de desenvolvimento web, onde decidi focar todas as minhas energias para me tornar uma pessoa desenvolvedora. 
+                <p>Rebobinando um pouquinho mais a trilha do tempo, meu interesse foi despertado ainda dentro do âmbito institucional quando tive contato com discussões que abarcavam o escopo do racismo algorítmico, e digamos que minhas anteninhas ficaram atentas ao fenômeno em questão. Após ter pesquisado muito sobre o tema e revisitado diversos pontos de vista, indo de tecnologistas à cientistas sociais, encorpei minha questão formulada inicialmente, todavia, ainda não tenho as respostas para as minhas incógnitas.  Contudo, a possibilidade de beber da fonte de outras áreas para a analisar a composição de um fenômeno político-social verdadeiramente me instigou.  </p>
+                
+                <p><span> Pense em um mosaico</span>. É exatamente dessa forma que eu enxergo a reconstrução interdisciplinar, pois apesar de cada elemento possuir suas características próprias é na harmonia (às vezes nem tão harmoniosa) que algo muito mais robusto é gerado. É na conexão que pode-se alcançar a <span>inovação</span> por meio de <span>valores ressignificados</span>, e é exatamente isso que faz os meus olhos brilharem na área tecnológica.  </p>
+                    
+                <p>Em suma, posso considerar que no meu tempo livre gosto de estudar sobre algoritmos e inteligencia artificial, com ênfase nos vieses de reconhecimento facial. Contudo, o que realmente me cativou foi a área de desenvolvimento web, decidindo assim focar todas as minhas energias para me tornar uma pessoa desenvolvedora. 
                     Digo de antemão que até chegar ao ponto de conseguir me visualizar dentro desse espectro foi um longo processo, mas justamente por conta do vínculo às comunidades digitais consegui visualizar futuros possíveis. E agora que o <span>pilar de autoconhecimento </span>foi desenvolvido nada me fará questionar acerca do meu valor e competência para conquistar o que quero.</p>
 
             </div>
@@ -35,12 +37,13 @@ function Jornada(){
             </div>
 
             <div className={styles.texto}>
-                <p>Certamente as comunidades digitais tiveram um papel crucial para que o impulso fosse tomado e acredito que palavras não seriam suficientes para expressar tamanha gratidão, em especial às comunidades compostas por pessoas negras. </p>
+                <p>Certamente as comunidades digitais tiveram um papel crucial para que o impulso fosse tomado, acredito que palavras não seriam suficientes para expressar tamanha gratidão, em especial às comunidades compostas por pessoas negras. </p>
             </div>
 
             <div className={styles.sobre}>
                 <h2><i>Redescobrir o gosto e o sabor da festa</i></h2>
-                <h5>Verso da música <span>Redescobrir</span> (amo a versão na voz da Maria Rita, filha da Elis Regina).</h5>
+                <h5>Verso da música <a href="https://www.youtube.com/watch?v=CsiH25p_rAk" target="_blank" ><span>Redescobrir </span> </a>
+                ( amo a versão na voz da Maria Rita, filha da Elis Regina ).</h5>
             </div>
 
             <div className={styles.texto}>
